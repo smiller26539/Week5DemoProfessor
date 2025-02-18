e924fab309025b1e125a3bf279404084e180f9c6
smiller26539 <smiller26@ggc.edu>
2025-02-18 16:44:15 -0500

    I got nothing I'm sorry :(

--- Prof_3DMovement/Assets/Scripts/InvMgr.cs
+++ Prof_3DMovement/Assets/Scripts/InvMgr.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class InvMgr : MonoBehaviour
@@ -7,6 +8,13 @@ public class InvMgr : MonoBehaviour
     public GameObject[] allItems;
 
     public int currentItemIndex;
+
+    Image iconImg;
+
+    void Start()
+    {
+        iconImg = GameObject.Find("Icon").GetComponent<Image>();   
+    }
 
     public GameObject getShotType() {
         if(currentItemIndex < 0) {
@@ -16,5 +24,14 @@ public class InvMgr : MonoBehaviour
             return allItems[currentItemIndex];
         }
 
+    }
+
+    public void setShotType(int itemIndex)
+    {
+        currentItemIndex = itemIndex;
+
+        iconImg.enabled = true;
+
+        iconImg.color = Color.blue;
     }
 }
